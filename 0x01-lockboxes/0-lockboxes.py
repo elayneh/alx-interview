a84590711fcd8d6ece3547a221d86942d82d1682
elayneh <betenatefera@gmail.com>
2022-06-23 22:24:31 +0300

    documentation is added

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
+"""
+Problem: You have n number of locked boxes in front of you.
+         Each box is numbered sequentially from 0 to n - 1
+         and each box may contain keys to the other boxes.
+Task: Write a method that determines if all the boxes can be opened or not.
+"""
+
+
 def canUnlockAll(boxes):
-    
+    """
+    Function that checks with boolean value if the list type and
+    length to invoke two for iterations one to traverse the list
+    and the other to compaer if key is idx or not in order to open
+    """
     if type(boxes) is not list:
         return False
     elif (len(boxes)) == 0:
