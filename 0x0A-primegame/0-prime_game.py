a4f09a85893b32d847dd29e8af2aacfdb391719f
elayneh <betenatefera@gmail.com>
2022-08-31 11:00:08 +0300

    correct

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+""" Module for Prime Game """
 
 
 def isWinner(x, nums):
+    """Solves Prime Game"""
     if not nums or x < 1:
         return None
     n = max(nums)
-    List = []
-    for _ in range(max(n+1, 2)):
-        List.append(True)
+    sieve = [True for _ in range(max(n + 1, 2))]
     for i in range(2, int(pow(n, 0.5)) + 1):
-        if not List[i]:
+        if not sieve[i]:
             continue
         for j in range(i*i, n + 1, i):
-            List[j] = False
+            sieve[j] = False
 
-    List[0] = List[1] = False
+    sieve[0] = sieve[1] = False
     c = 0
-    for i in range(len(List)):
-        if List[i]:
+    for i in range(len(sieve)):
+        if sieve[i]:
             c += 1
-        List[i] = c
+        sieve[i] = c
 
     player1 = 0
     for n in nums:
-        player1 += List[n] % 2 == 1
+        player1 += sieve[n] % 2 == 1
     if player1 * 2 == len(nums):
         return None
     if player1 * 2 > len(nums):
